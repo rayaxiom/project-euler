b55961331638ece7235a5160cb46c5f8656a73ed
Raymon White <raymonwhite42@gmail.com>
2012-12-28 02:17:18 +0000

    Moved helper functions into a header

--- src_problem/p0001.cpp
+++ src_problem/p0001.cpp
@@ -15,29 +15,16 @@
  */
 #include <iostream>
 #include <cstdlib>
-#include<cmath>
-
-// Sum of natural numbers from 0.
-unsigned long sum_until(unsigned long N)
-{
-  return 0.5 * N * (N+1);
-}
-
-// Sum  multiples
-unsigned long sum_of_multiples_until(unsigned multiple, unsigned max)
-{
-  unsigned long new_max = max / multiple;
-  return sum_until(new_max) * multiple;
-}
-
+#include <cmath>
+#include "ray_maths.h"
 
 // Find the sum of all the multiples of 3 or 5 below 1000.
 int main(int argc, char* argv[])
 {
   unsigned long max = 1000-1; // BELOW 1000
-  unsigned long answer = sum_of_multiples_until(3,max) 
-                         + sum_of_multiples_until(5,max)
-                         - sum_of_multiples_until(15,max);
+  unsigned long answer = raymaths::sum_of_multiples_until(3,max) 
+                         + raymaths::sum_of_multiples_until(5,max)
+                         - raymaths::sum_of_multiples_until(15,max);
   std::cout << answer << std::endl;
 
   return EXIT_SUCCESS;
---
+++ src_problem/ray_maths.h
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ray_maths.h
+ *
+ *    Description:  Mathematical helpers
+ *
+ *        Version:  1.0
+ *        Created:  28/12/12 02:06:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Raymon White, http://ilearnsum.blogspot.co.uk/ 
+ *
+ * =====================================================================================
+ */
+
+namespace raymaths
+{
+  // Sum of natural numbers starting from 1 until N
+  unsigned long sum_until(unsigned long N)
+  {
+   return 0.5 * N * (N+1);
+  }
+
+  // Sum  multiples until max.
+  // i.e. multiples of 2 until 10 is 2+4+6+8+10
+  unsigned long sum_of_multiples_until(unsigned multiple, unsigned max)
+  {
+   unsigned long new_max = max / multiple;
+   return sum_until(new_max) * multiple;
+  }
+
+}
+
+
+
