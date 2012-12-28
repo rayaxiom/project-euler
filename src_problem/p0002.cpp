8cf7128ca054eae698d6e57cce18fbed5987acde
Raymon White <raymonwhite42@gmail.com>
2012-12-28 01:22:41 +0000

    Initial setup

---
+++ src_problem/p0002.cpp
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  peproblem.cc
+ *
+ *    Description:  Problem 1
+ *
+ *        Version:  1.0
+ *        Created:  27/12/12 22:05:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Raymon White, 
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <cstdlib>
+//
+int main(int argc, char* argv[])
+{
+  std::cout << "kuuuuuuu" << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+
---
+++ src_problem/run.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BIN_DIR="./../bin/"
+
+"$BIN_DIR"$1
+
+
